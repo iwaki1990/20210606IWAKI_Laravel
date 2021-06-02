5bb29165b46ddd56155ee7d0365643fb9e2921bc
iwaki1990 <low.son.wing@gmail.com>
2021-06-02 20:08:23 +0900

    2 commit

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<form action="result.php" method="post">
+  お名前：<input type="text" name="my_name" />
+  <br>
+  ご希望商品 :
+   <input type ="radio" name="choices" value="Aセット"/>Aセット
+   <input type ="radio" name="choices" value="Bセット"/>Bセット
+   <input type ="radio" name="choices" value="Cセット"/>Cセット
+  <br>
+  注文数：<select name="number">
+      <?php for ($i=1;$i<=10;$i++){ ?>
+        <option value="<?php echo $i; ?>">
+          <?php echo $i; ?>
+        </option>
+      <?php } ?>
+    </select>
+  <br>
+  <input type="submit" value="送信" />
+</form>
