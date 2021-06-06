1f5cfcd3815d661f39639df66be2c697b7a1fa09
iwaki1990 <low.son.wing@gmail.com>
2021-06-06 19:30:33 +0900

    1 commit

---
+++ laravelpj/routes/web.php
@@ -0,0 +1,3 @@
+Route::get('/hello', function () {
+    return view('hello.index');
+});
--- mydb.php
+++ mydb.php
@@ -1,5 +1,5 @@
 <?php
-    
+ 
 $dsn = 'mysql:dbname=mydb;host=localhost';
 $user = 'root';
 $password = '';
@@ -10,11 +10,8 @@ try {
   print("接続に成功しました。<br>");
 } catch (PDOException $e) {
   print('Error:'.$e->getMessage());
+  die();
 }
 
-//追加
-$count = $dbh->exec('INSERT INTO items SET id=1, item_name="peach",price=210, keyword="缶詰,ピンク,甘い"');
-echo $count . '件のデータを挿入しました';
-//追加終わり
+$dbh = new PDO($dsn, $user, $password);
 
-$dbh = null;
