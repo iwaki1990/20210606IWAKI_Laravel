24229ba4677875f88cf32e1d7798b053591c8833
iwaki1990 <low.son.wing@gmail.com>
2021-06-07 18:16:02 +0900

    2commit

--- laravelpj/app/Http/Controllers/Controller.php
+++ laravelpj/app/Http/Controllers/Controller.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
-class Controller extends BaseController
+class HelloController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index()
+    {
+        $data = [
+            'content' => '本文',
+        ];
+        return view('hello.index', $data);
+    }
 }
--- laravelpj/resources/views/index.blade.php
+++ laravelpj/resources/views/index.blade.php
@@ -3,13 +3,18 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>COACHTECH</title>
+  <style>
+    p {
+      font-size: "16px";
+    }
+  </style>
 </head>
 
 <body>
   <h1>タイトル</h1>
+  <p>{{$content}}</p>
 </body>
 
 </html>
